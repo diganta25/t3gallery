b652f3f16d7acd612d1d3bfa60c376fad58584b7
stroboveski <stroboveski@gmail.com>
2024-07-15 03:25:48 +0530

    Added auth

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,19 @@
+import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/nextjs'
+
+export function TopNav(){ 
+    return (
+      <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b border-rose-600">
+        <div>Gallery</div>
+  
+        <div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton/>
+            </SignedIn>
+        </div>
+      </nav>
+    )
+  }
+  
