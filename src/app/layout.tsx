4144a6e4e7040c08fe284e419a471ef84e438add
stroboveski <stroboveski@gmail.com>
2024-07-15 03:32:09 +0530

    fixed css error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import './globals.css';
+
 
 import { TopNav } from "./_components/topnav";
 
