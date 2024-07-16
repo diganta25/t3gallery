3f6d1255f9b7327b68e8fa8f3fad6f1a007883ae
stroboveski <stroboveski@gmail.com>
2024-07-16 20:25:03 +0530

    more cleanup on modal

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,13 +1,17 @@
 import { getImages } from "~/server/queries";
 
+export default async function FullPageImageView(props: { id: number }) {
+  if (Number.isNaN(props.id)) throw new Error("Invalid photo id");
 
-export default async function FullPageImageView(props: {id: number}) {
-
-
-  if (Number.isNaN(props.id)) throw new Error("Invalid photo id")
-
-  const image = await getImages(props.id)
+  const image = await getImages(props.id);
   return (
-    <img src={image.url} className="w-96" />
-  )
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex-shrink flex justify-center items-center">
+        <img src={image.url} className="flex-shrink object-contain" />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
