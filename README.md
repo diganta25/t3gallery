0ca0af347f9c09cb94760e6436904bf3cfed2675
stroboveski <stroboveski@gmail.com>
2024-07-17 16:03:23 +0530

    Basic posthog analytics

--- README.md
+++ README.md
@@ -14,8 +14,8 @@
  - [x] Error management (w/ Sentry)
  - [x] Routing/image page (parallel route)
  - [x] Update upload button to be less cringe
- - [ ] ShadUI (specifically toasts)
- - [ ] Delete button (w/server actions)
+ - [x] ShadUI (specifically toasts)
  - [ ] Analytics (posthog)
+ - [ ] Delete button (w/server actions)
  - [ ] Ratelimiting (upstash)
  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 import "@uploadthing/react/styles.css";
 
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { TopNav } from "./_components/topnav";
 
@@ -11,6 +11,7 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
 import { Toaster } from "~/components/ui/sonner";
+import { CSPostHogProvider } from "./_analitics/provider";
 
 export const metadata: Metadata = {
   title: "t3 Gallery",
@@ -18,37 +19,37 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-
 export default function RootLayout({
   children,
-  modal
-}: Readonly<{ 
-              children: React.ReactNode;   
-              modal: React.ReactNode; 
-            }>) 
-  {
+  modal,
+}: Readonly<{
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}>) {
   return (
     <ClerkProvider>
-    <html lang="en" className={`${GeistSans.variable}`}>
-    <NextSSRPlugin
-          /**
-           * The `extractRouterConfig` will extract **only** the route configs
-           * from the router to prevent additional information from being
-           * leaked to the client. The data passed to the client is the same
-           * as if you were to fetch `/api/uploadthing` directly.
-           */
-          routerConfig={extractRouterConfig(ourFileRouter)}
-        />
-      <body className="dark">
-        <div className="h-screen grid grid-rows-[auto,1fr]">
-          <TopNav/>
-          <main className="overflow-y-scroll">{children}</main>
-          </div>
-        {modal}
-        <div id="modal-root" />
-        <Toaster />
-      </body>
-    </html>
+      <CSPostHogProvider>
+        <html lang="en" className={`${GeistSans.variable}`}>
+          <NextSSRPlugin
+            /**
+             * The `extractRouterConfig` will extract **only** the route configs
+             * from the router to prevent additional information from being
+             * leaked to the client. The data passed to the client is the same
+             * as if you were to fetch `/api/uploadthing` directly.
+             */
+            routerConfig={extractRouterConfig(ourFileRouter)}
+          />
+          <body className="dark">
+            <div className="grid h-screen grid-rows-[auto,1fr]">
+              <TopNav />
+              <main className="overflow-y-scroll">{children}</main>
+            </div>
+            {modal}
+            <div id="modal-root" />
+            <Toaster />
+          </body>
+        </html>
+      </CSPostHogProvider>
     </ClerkProvider>
   );
 }
