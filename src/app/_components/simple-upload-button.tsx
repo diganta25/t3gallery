be86863f05f3444064e2bc637a5674b30581d35f
stroboveski <stroboveski@gmail.com>
2024-07-17 12:31:55 +0530

    add shad sonner toast component

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "../utils/uploadthing";
+import { toast } from "sonner"
+
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -34,7 +36,15 @@ export function SimpleUploadButton(){
   const router = useRouter();
 
   const {inputProps} = useUploadThingInputProps("imageUploader", {
+    onUploadBegin() {
+      toast("Uploading ...", {
+        duration: 100000,
+        id: "upload-begin",
+      });
+    },
     onClientUploadComplete() {
+      toast.dismiss("upload-begin");
+      toast("Upload Complete!")
       router.refresh();
     }
   });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { type Metadata } from "next";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
+import { Toaster } from "~/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "t3 Gallery",
@@ -45,6 +46,7 @@ export default function RootLayout({
           </div>
         {modal}
         <div id="modal-root" />
+        <Toaster />
       </body>
     </html>
     </ClerkProvider>
